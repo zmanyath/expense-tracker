ef45767967c424109e380836a1765dc412b9f0db
zanele <zanelemanyath@gmail.com>
2023-07-16 23:19:51 +0200

    Fixed Nav mistake

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -6,10 +6,10 @@ const TopBar = () => {
         <div>
             <nav className="bg-purple-300 text-lg border-gray-200 px-4 lg:px-8 py-2.5 dark:bg-[#a78bfa]">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                <a href="/Home" className="flex items-center">
+                <NavLink to='/Home' className="flex items-center">
                     <img src={logo} className="mr-3 h-6 sm:h-9" alt="" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap">Expenser!</span>
-                </a>
+                </NavLink>
                 <div className="flex items-center lg:order-2">
                     <NavLink to='/'><span className="text-gray-800 dark:text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log In</span></NavLink>
                 </div>
